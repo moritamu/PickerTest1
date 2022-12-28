d9e7bd0911568685c0acc9fd4758424599dc406c
moritamu <moris@r.sannet.ne.jp>
2022-12-28 22:53:27 +0900

    Initial Commit

---
+++ PickerTest1/PickerTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTest1App.swift
+//  PickerTest1
+//
+//  Created by 森田晋 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct PickerTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
