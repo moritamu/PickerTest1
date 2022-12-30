8cafa16661feb0a7b28822f48488c395e123fc55
moritamu <moris@r.sannet.ne.jp>
2022-12-30 09:14:30 +0900

    ForEachを使った。 警告も解除した。 Identifiableプロトコルに準拠させた。

--- PickerTest1/ContentView.swift
+++ PickerTest1/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let fruits = ["みかん","りんご","かき","バナナ","キウイ"]
+    //    配列にすることによって、数字でなくて内容を表示できるようになる
+    @State private var selection = 3
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Picker(selection: $selection, label: Text("フルーツを選択")) {
+                //            一つ一つ指定した方法
+                //                Text("みかん").tag(1)//tagで指定した値がselectionに入る
+                //                Text("りんご").tag(2)
+                //                Text("バナナ").tag(3)
+                //                Text("かき").tag(4)
+                //                Text("キウイ").tag(5)
+                //                ForEach(0..<fruits.count, id: \.self) { num in
+                ForEach(fruits.indices, id: \.self) { num in
+                    Text(fruits[num])
+                    //Non-constant range: argument must be an integer literal
+                    //という warning が表示されるため、ForEachにid: \.selfを入れた。
+                    //fruits.indices は 0..<fruits.count と同じ
+                }
+            }
+            .pickerStyle(.segmented)//これでPickerのスタイルを変更できる。
+            .frame(width: 300)
+            Text("選択肢：\(fruits[selection])")
+            //            selectionには配列の０からの番号が入っている。
+                .foregroundColor(.green)
+                .font(.title2)
         }
         .padding()
+        
     }
 }
 
